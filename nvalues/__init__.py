70fdbe60656845d34fbd310fce3294010984b3d7
Cariad Eccleston <cariad@cariad.earth>
2022-11-23 08:53:20 +0000

    Add Line class

--- nvalues/__init__.py
+++ nvalues/__init__.py
@@ -4,11 +4,13 @@ nvalues is a Python package for working with n-dimensional volumes of data.
 
 from importlib.resources import files
 
-from nvalues.values import Values
+from nvalues.line import Line
+from nvalues.volume import Volume
 
 with files(__package__).joinpath("VERSION").open("r") as t:
     version = t.readline().strip()
 
 __all__ = [
-    "Values",
+    "Line",
+    "Volume",
 ]
---
+++ tests/test_line.py
@@ -0,0 +1,11 @@
+from nvalues import Line
+
+
+def test_default() -> None:
+    assert Line[int, str]("default").get(0) == "default"
+
+
+def test_set() -> None:
+    line = Line[int, str]("default")
+    line.set(0, "foo")
+    assert line.get(0) == "foo"
