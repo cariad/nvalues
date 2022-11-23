54e8eadba46e961c615040edb3d1d29465f720df
Cariad Eccleston <cariad@cariad.earth>
2022-11-23 15:44:16 +0000

    Add KeyIndexOutOfRange

--- nvalues/exceptions.py
+++ nvalues/exceptions.py
@@ -1,6 +1,23 @@
 from typing import Any
 
 
+class KeyIndexOutOfRange(LookupError):
+    """
+    Raised when requesting a value by its index and that index is out of range.
+    """
+
+    def __init__(self, key_depth: int) -> None:
+        self._key_depth = key_depth
+
+    @property
+    def key_depth(self) -> int:
+        """
+        Depth of the key that fell outside of its range.
+        """
+
+        return self._key_depth
+
+
 class NKeyError(LookupError):
     """
     Raised when requesting the value of a key that doesn't exist and a default
