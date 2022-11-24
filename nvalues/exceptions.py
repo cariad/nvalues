2b406ac88ee9976e9c3525902b29a372d07540d7
Cariad Eccleston <cariad@cariad.earth>
2022-11-24 14:04:22 +0000

    Delete NoDefaultValue

--- nvalues/exceptions.py
+++ nvalues/exceptions.py
@@ -54,12 +54,3 @@ class NKeyError(LookupError):
     def __init__(self, keys: tuple[Any, ...], key_index: int) -> None:
         msg = f"Key {key_index} of {keys} ({keys[key_index]}) does not exist"
         super().__init__(msg)
-
-
-class NoDefaultValue(Exception):
-    """
-    Raised when requesting a volume's default value when one hasn't been set.
-    """
-
-    def __init__(self) -> None:
-        super().__init__("The volume does not have a default value")
